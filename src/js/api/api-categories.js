eb9faf4fc6f587d4dc06388c2945ec2c7d40c562
Max Banin <banin@ukr.net>
2026-06-13 03:09:03 +0300

    pet-list 1 iteration

---
+++ src/js/api/api-categories.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://paw-hut.b.goit.study';
+const CATEGORIES = '/api/categories';
+const ANIMALS = '/api/animals';
+
+export async function getCategories() {
+  const res = await axios.get(BASE_URL + CATEGORIES);
+  return res.data;
+}
+
+export async function getCategory(id, numPage, count) {
+  const params = {
+    page: numPage,
+    limit: count,
+  };
+  if (id !== 'all') params.categotyid = id;
+  const res = await axios.get(BASE_URL + ANIMALS, { params });
+  return res.data;
+}
