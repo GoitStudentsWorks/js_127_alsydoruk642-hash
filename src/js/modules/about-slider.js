ed207c637a91f173a4b8b1e81a4d0df1b3c0cead
Olexandr Kuzub <alexkuzub25@gmail.com>
2026-06-14 23:49:21 +0300

    fix css

--- src/js/modules/about-slider.js
+++ src/js/modules/about-slider.js
@@ -1,42 +1,34 @@
-// core version + navigation, pagination modules:
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-// init Swiper:
-const aboutUsSwiper = new Swiper('.swiper', {
-  // configure Swiper to use modules
-  modules: [Navigation, Pagination],
+const aboutUsSection = document.querySelector('.about-us');
 
-  navigation: {
-    nextEl: '.swiper-button-next.about-us-button-next',
-    prevEl: '.swiper-button-prev.about-us-button-prev',
-  },
+const aboutUsSwiper = new Swiper(
+  aboutUsSection.querySelector('.about-us-swiper'),
+  {
+    modules: [Navigation, Pagination],
 
-  pagination: {
-    el: '.about-us-swiper-pagination',
-    clickable: true,
-    dynamicBullets: window.innerWidth < 768,
-  },
-});
+    navigation: {
+      nextEl: aboutUsSection.querySelector('.about-us-button-next'),
+      prevEl: aboutUsSection.querySelector('.about-us-button-prev'),
+    },
 
-const mediaQuery = window.matchMedia('(max-width: 767px)'); // для мобільних пристроїв
+    pagination: {
+      el: aboutUsSection.querySelector('.about-us-swiper-pagination'),
+      clickable: true,
+      dynamicBullets: window.innerWidth < 768,
+    },
+  }
+);
 
-function updatePagination(e) {
-  console.log('matches:', e.matches);
+// оновлення dynamicBullets без destroy/init (ВАЖЛИВО)
+window.addEventListener('resize', () => {
+  const isMobile = window.innerWidth < 768;
 
-  aboutUsSwiper.params.pagination.dynamicBullets = e.matches;
-
-  aboutUsSwiper.pagination.destroy();
-  aboutUsSwiper.pagination.init();
-  aboutUsSwiper.pagination.render();
+  aboutUsSwiper.params.pagination.dynamicBullets = isMobile;
   aboutUsSwiper.pagination.update();
-
-  console.log(aboutUsSwiper.params.pagination.dynamicBullets);
-}
-
-updatePagination(mediaQuery);
-mediaQuery.addEventListener('change', updatePagination);
+});
